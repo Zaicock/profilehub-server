644bdaec70b43217032ba97cd279b9fb13c09847
dev ghazawi vip <81128163+Zaicock@users.noreply.github.com>
2025-12-15 05:49:35 +0300

    Update server.js new

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET || "dev_secret";
 const express = require('express');
 const WebSocket = require('ws');
 const http = require('http');
