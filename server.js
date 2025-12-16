d1bf666b16e1f4e54734a6e8e20597c42a2920fa
dev ghazawi vip <81128163+Zaicock@users.noreply.github.com>
2025-12-16 14:14:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ async function createTablesIfNotExist() {
   `);
 
   await pool.execute(`
-    CREATE TABLE IF NOT EXISTS userspro (
+    CREATE TABLE IF NOT EXISTS users (
       id INT AUTO_INCREMENT PRIMARY KEY,
       username VARCHAR(50) UNIQUE NOT NULL,
       email VARCHAR(100) UNIQUE NOT NULL,
