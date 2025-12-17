b42591a2cc4ce740624f461d6b4861a2b758f050
dev ghazawi vip <81128163+Zaicock@users.noreply.github.com>
2025-12-17 09:54:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 const server = http.createServer(app);
 
 // ================== DATABASE CONFIGURATION ==================
